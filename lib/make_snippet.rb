ac9ceafa01b041fbb430dc84a216f457f199bcbe
Molly Gallagher Wickham <mollyg210@gmail.com>
2022-10-03 14:08:48 +0100

    make snippet commit

---
+++ lib/make_snippet.rb
@@ -0,0 +1,15 @@
+# A method called make_snippet that takes a string as an argument and returns the first five words and then a '...' 
+# if there are more than that.
+
+def make_snippet(string)
+    words = string.split(" ")
+    # if string is more than 5 words
+    
+    if words.length >= 5
+        words.first(5).join(" ") + "..."
+    # if string is fewer than 5 words
+    else   
+        return string
+    end
+
+end
