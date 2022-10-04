cfa9cf941dc3b7c46e96b54d72e02a5d9cb6b992
Molly Gallagher Wickham <mollyg210@gmail.com>
2022-10-04 12:28:45 +0100

    personal diary challenge commit

--- lib/personal_diary.rb
+++ lib/personal_diary.rb
@@ -1,46 +1,63 @@
 # A method called make_snippet that takes a string as an argument and returns the first five words and then a '...' 
 # if there are more than that.
-
-def make_snippet(string)
-    words = string.split(" ")
-    regex = /[!.,:;?@$%&)]/
-    # if string is more than 5 words
-    # if last character of fifth word is punctuation, delete last character
-    if words.length >= 5 && words[4] =~ regex
-        last_word = words[4].gsub(regex, "...")
-        return words.first(4).join(" ") + " " +last_word
-    elsif words.length >=5
-        return words.first(5).join(" ") + "..."
-    # if string is fewer than 5 words
-    else   
-        return string
+class PersonalDiary 
+    
+    def make_snippet(string)
+        words = string.split(" ")
+        regex = /[!.,:;?@$%&)]/
+        # if string is more than 5 words
+        # if last character of fifth word is punctuation, delete last character
+        if count_words(string) >= 5 && words[4] =~ regex
+            last_word = words[4].gsub(regex, "...")
+            return words.first(4).join(" ") + " " +last_word
+        elsif count_words(string) >=5
+            return words.first(5).join(" ") + "..."
+        # if string is fewer than 5 words
+        else   
+            return string
+        end
     end
-end
 
-
-def count_words(string)
-    # if string == ""
-    #     return 0
-    # else
-    #     return string.split(" ").length
-    # end
-    string == "" ? 0 : string.split(" ").length
-end
-
-def reading_time(text)
-    # count_words(text) -> will give us total number of words
-    words_number = count_words(text)
-    if words_number < 200
-        return "Estimated reading time is less than 1 minute"
-    elsif words_number == 200
-        return "Estimated reading time is 1 minute"
-    elsif (words_number / 200) < 60
-        minutes = words_number / 200 
-        return "Estimated reading time is #{minutes.ceil} minutes"
-    else 
-        minutes = words_number / 200
-        hours = minutes / 60
-        return "Estimated reading time is at least #{hours.ceil} hour"
+    def count_words(string)
+        string.split(" ").length
     end
-    # then work out estimated time (% - use to work out estimated)
+
+    def reading_time(text)
+        number = count_words(text)
+        hours = (number / 12000)
+        minutes = (number / 200)
+        seconds = ((number % 200) / 3).round(2)
+        if number < 200
+            return "Estimated reading time is #{seconds} second(s)"
+        elsif number < 12000
+            return "Estimated reading time is #{minutes} minute(s) and #{seconds} second(s)"
+        elsif number % 12000 == 0
+            return "Estimated reading time is #{hours} hour(s), 0 minute(s) and 0 second(s)"
+        else 
+            return "Estimated reading time is #{hours} hour(s), #{minutes} minute(s) and #{seconds} second(s)"
+        end
+    end
+
+#     As a user
+#     So that I can keep track of my tasks
+#     I want to check if a text includes the string #TODO.
+
+# check_to_do(text)
+# text: a string (e.g. #TODO walk the dogs)
+
+# check_to_do("#TODO walk the dogs") => "Your tasks: walk the dogs"
+# check_to_do("#TODO walk the dogs, #TODO wash the dishes") => "Your tasks: walk the dogs, wash the dishes"
+# check_to_do("Lorem ipsum dolor sit amet") => "There are no tasks given."
+# check_to_do(""") => "There are no tasks given."
+# check_to_do(nil) => throws an error
+    def check_to_do(text)
+        if text.nil?
+            fail "No text given."
+        elsif text.include?("#TODO")
+            task = text.delete("#TODO").split(",").map{|x| x.strip}.join(", ")
+            "Your tasks: #{task}"
+        else
+            "There are no tasks given."
+        end
+    end
 end
--- spec/personal_diary_spec.rb
+++ spec/personal_diary_spec.rb
@@ -3,68 +3,109 @@
 
 require 'personal_diary'
 
-RSpec.describe "make_snippet method" do
+RSpec.describe PersonalDiary do
     it "returns first 5 words followed by '...' " do
-        result = make_snippet("Once upon a time a long time ago")
+        entry = PersonalDiary.new
+        result = entry.make_snippet("Once upon a time a long time ago")
         expect(result).to eq("Once upon a time a...")
 
     end
 
     it "returns first 5 words followed by '...' while ignoring end of fifth word punctuation " do
-        result = make_snippet("now, it was too purr-fect! Pee in the shoe")
+        entry = PersonalDiary.new
+        result = entry.make_snippet("now, it was too purr-fect! Pee in the shoe")
         expect(result).to eq("now, it was too purr-fect...")
     end
 
 
     it "returns whole string if fewer than 5 words" do
-        result = make_snippet("Lived happily ever after")
+        entry = PersonalDiary.new
+        result = entry.make_snippet("Lived happily ever after")
         expect(result).to eq("Lived happily ever after")
     end
 
     it "returns first 5 words including punctuation" do
-        result = make_snippet("It's 3am, time to create some chaos present belly, scratch hand when stroked litter box is life,")
+        entry = PersonalDiary.new
+        result = entry.make_snippet("It's 3am, time to create some chaos present belly, scratch hand when stroked litter box is life,")
         expect(result).to eq("It's 3am, time to create...")
     end
 
-end
-
 # A method called count_words that takes a string as an argument and returns the number of words in that string.
-RSpec.describe "count_words method" do
+
     it "returns 0 if the string is empty" do
-        result = count_words("")
+        entry = PersonalDiary.new
+        result = entry.count_words("")
         expect(result).to eq 0
     end
 
     it "given a string of 3 words it returns 3" do
-        result = count_words("time to create")
+        entry = PersonalDiary.new
+        result = entry.count_words("time to create")
         expect(result).to eq 3
     end
-end
 
-RSpec.describe "reading time method" do
-    it "given a text of less than 200 words, read in less than 1 minute" do
-        text = "Cat ipsum dolor sit amet"
-        result = reading_time(text)
-        expect(result).to eq "Estimated reading time is less than 1 minute"
+    it "given a text of 10 words, read in less than 3 seconds" do
+        entry = PersonalDiary.new
+        text = "Cat ipsum dolor sit amet Cat ipsum dolor sit amet"
+        result = entry.reading_time(text)
+        expect(result).to eq "Estimated reading time is 3 second(s)"
+    end
+
+    it "given a text of 20 words, read in estimated 6 seconds" do
+        entry = PersonalDiary.new
+        text = "Cat ipsum dolor sit amet Cat ipsum dolor sit amet Cat ipsum dolor sit amet Cat ipsum dolor sit amet"
+        result = entry.reading_time(text)
+        expect(result).to eq "Estimated reading time is 6 second(s)"
     end
 
     it "given a text of 200 words, read in 1 minute" do
+        entry = PersonalDiary.new
         text = "Plays league of legends stare out the window. Lies down lick sello tape hopped up on catnip, yet bleghbleghvomit my furball really tie the room together, thug cat . Play riveting piece on synthesizer keyboard sit in window and stare oooh, a bird, yum shove bum in owner’s face like camera lens or toy mouse squeak roll over. Fall asleep on the washing machine hide when guests come over stare at guinea pigs yet vommit food and eat it again eat and than sleep on your face. Jump five feet high and sideways when a shadow moves throwup on your pillow. Plays league of legends stare out the window. Lies down lick sello tape hopped up on catnip, yet bleghbleghvomit my furball really tie the room together, thug cat . Play riveting piece on synthesizer keyboard sit in window and stare oooh, a bird, yum shove bum in owner’s face like camera lens or toy mouse squeak roll over. Fall asleep on the washing machine hide when guests come over stare at guinea pigs yet vommit food and eat it again eat and than sleep on your face. Jump five feet high and sideways when a shadow moves throwup on."
-        result = reading_time(text)
-        expect(result).to eq "Estimated reading time is 1 minute"
+        result = entry.reading_time(text)
+        expect(result).to eq "Estimated reading time is 1 minute(s) and 0 second(s)"
     end
 
     it "given a text of 400 words, read in 2 minutes" do
+        entry = PersonalDiary.new
         text = "Plays league of legends stare out the window. Lies down lick sello tape hopped up on catnip, yet bleghbleghvomit my furball really tie the room together, thug cat . Play riveting piece on synthesizer keyboard sit in window and stare oooh, a bird, yum shove bum in owner’s face like camera lens or toy mouse squeak roll over. Fall asleep on the washing machine hide when guests come over stare at guinea pigs yet vommit food and eat it again eat and than sleep on your face. Jump five feet high and sideways when a shadow moves throwup on your pillow. Plays league of legends stare out the window. Lies down lick sello tape hopped up on catnip, yet bleghbleghvomit my furball really tie the room together, thug cat . Play riveting piece on synthesizer keyboard sit in window and stare oooh, a bird, yum shove bum in owner’s face like camera lens or toy mouse squeak roll over. Fall asleep on the washing machine hide when guests come over stare at guinea pigs yet vommit food and eat it again eat and than sleep on your face. Jump five feet high and sideways when a shadow moves throwup on. Plays league of legends stare out the window. Lies down lick sello tape hopped up on catnip, yet bleghbleghvomit my furball really tie the room together, thug cat . Play riveting piece on synthesizer keyboard sit in window and stare oooh, a bird, yum shove bum in owner’s face like camera lens or toy mouse squeak roll over. Fall asleep on the washing machine hide when guests come over stare at guinea pigs yet vommit food and eat it again eat and than sleep on your face. Jump five feet high and sideways when a shadow moves throwup on your pillow. Plays league of legends stare out the window. Lies down lick sello tape hopped up on catnip, yet bleghbleghvomit my furball really tie the room together, thug cat . Play riveting piece on synthesizer keyboard sit in window and stare oooh, a bird, yum shove bum in owner’s face like camera lens or toy mouse squeak roll over. Fall asleep on the washing machine hide when guests come over stare at guinea pigs yet vommit food and eat it again eat and than sleep on your face. Jump five feet high and sideways when a shadow moves throwup on."
-        result = reading_time(text)
-        expect(result).to eq "Estimated reading time is 2 minutes"
+        result = entry.reading_time(text)
+        expect(result).to eq "Estimated reading time is 2 minute(s) and 0 second(s)"
     end
 
     it "given a text of 12000 words, read in 1 hour" do
+        entry = PersonalDiary.new
         text = "Plays league of legends stare out the window. Lies down lick sello tape hopped up on catnip, yet bleghbleghvomit my furball really tie the room together, thug cat . Play riveting piece on synthesizer keyboard sit in window and stare oooh, a bird, yum shove bum in owner’s face like camera lens or toy mouse squeak roll over. Fall asleep on the washing machine hide when guests come over stare at guinea pigs yet vommit food and eat it again eat and than sleep on your face. Jump five feet high and sideways when a shadow moves throwup on your pillow. Plays league of legends stare out the window. Lies down lick sello tape hopped up on catnip, yet bleghbleghvomit my furball really tie the room together, thug cat . Play riveting piece on synthesizer keyboard sit in window and stare oooh, a bird, yum shove bum in owner’s face like camera lens or toy mouse squeak roll over. Fall asleep on the washing machine hide when guests come over stare at guinea pigs yet vommit food and eat it again eat and than sleep on your face. Jump five feet high and sideways when a shadow moves throwup on. Plays league of legends stare out the window. Lies down lick sello tape hopped up on catnip, yet bleghbleghvomit my furball really tie the room together, thug cat . Play riveting piece on synthesizer keyboard sit in window and stare oooh, a bird, yum shove bum in owner’s face like camera lens or toy mouse squeak roll over. Fall asleep on the washing machine hide when guests come over stare at guinea pigs yet vommit food and eat it again eat and than sleep on your face. Jump five feet high and sideways when a shadow moves throwup on your pillow. Plays league of legends stare out the window. Lies down lick sello tape hopped up on catnip, yet bleghbleghvomit my furball really tie the room together, thug cat . Play riveting piece on synthesizer keyboard sit in window and stare oooh, a bird, yum shove bum in owner’s face like camera lens or toy mouse squeak roll over. Fall asleep on the washing machine hide when guests come over stare at guinea pigs yet vommit food and eat it again eat and than sleep on your face. Jump five feet high and sideways when a shadow moves throwup on. "
         new_text = text * 30
-        result = reading_time(new_text)
-        expect(result).to eq "Estimated reading time is at least 1 hour"
+        result = entry.reading_time(new_text)
+        expect(result).to eq "Estimated reading time is 1 hour(s), 0 minute(s) and 0 second(s)"
     end
+    
+    it "given text with TODO, return task" do
+        entry = PersonalDiary.new
+        text = "#TODO walk the dogs"
+        result = entry.check_to_do(text)
+        expect(result).to eq "Your tasks: walk the dogs"
+    end
+
+    it "given text with multiple TODO, return tasks" do
+        entry = PersonalDiary.new
+        text = "#TODO walk the dogs, #TODO wash the dishes"
+        result = entry.check_to_do(text)
+        expect(result).to eq "Your tasks: walk the dogs, wash the dishes"
+    end
+
+    it "given no TODO, return no tasks" do
+        entry = PersonalDiary.new
+        text = "Lorem ipsum dolor sit amet"
+        result = entry.check_to_do(text)
+        expect(result).to eq "There are no tasks given."
+    end
+
+    it "fails when text is nil" do
+        entry = PersonalDiary.new
+        text = nil
+        expect{entry.check_to_do(text)}.to raise_error "No text given."
+    end
+
 
 end
